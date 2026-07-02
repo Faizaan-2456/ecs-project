b56cc9143b3c0158fc56f112f622c84dd4711f57
Faizaan-2456 <faizanrashid820@gmail.com>
2026-07-02 16:48:03 +0100

    added variables

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -3,3 +3,15 @@ variable "region" {
   type = string
   default = "eu-west-2"
 }
+
+variable "az_1" {
+    description = "az for first subnet"
+    type = string
+    default = "eu-west-2a"
+}
+
+variable "az_2" {
+    description = "az for second subnet"
+    type = string
+    default = "eu-west-2b"
+}
