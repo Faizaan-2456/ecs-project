e0e5b2747d49f62de3c3fd2ec8fabf409d19e4da
Faizaan-2456 <faizanrashid820@gmail.com>
2026-07-02 16:47:21 +0100

    created vpc

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,5 +1,4 @@
-# Create VPC
-
+# Create vpc
 resource "aws_vpc" "ecs-vpc" {
   cidr_block       = "10.0.0.0/16"
   region = var.region
@@ -9,10 +8,11 @@ resource "aws_vpc" "ecs-vpc" {
 }
 
 
-# Create subnets
+# Create public subnets
 resource "aws_subnet" "public-subnet-1" {
   vpc_id     = aws_vpc.ecs-vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = var.az_1
 
   tags = {
     Name = "public-subnet-1"
@@ -21,27 +21,13 @@ resource "aws_subnet" "public-subnet-1" {
 resource "aws_subnet" "public-subnet-2" {
   vpc_id     = aws_vpc.ecs-vpc.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = var.az_2
 
   tags = {
     Name = "public-subnet-2"
   }
 }
-resource "aws_subnet" "private-subnet-1" {
-  vpc_id     = aws_vpc.ecs-vpc.id
-  cidr_block = "10.0.3.0/24"
 
-  tags = {
-    Name = "private-subnet-1"
-  }
-}
-resource "aws_subnet" "private-subnet-2" {
-  vpc_id     = aws_vpc.ecs-vpc.id
-  cidr_block = "10.0.4.0/24"
-
-  tags = {
-    Name = "private-subnet-2"
-  }
-}
 
 # Create Internet Gateway
 resource "aws_internet_gateway" "ecs-gw" {
@@ -52,7 +38,8 @@ resource "aws_internet_gateway" "ecs-gw" {
   }
 }
 
-# Create route tables
+
+# Create public route table and route to igw
 resource "aws_route_table" "public-rt" {
   vpc_id = aws_vpc.ecs-vpc.id
 
@@ -62,6 +49,18 @@ resource "aws_route_table" "public-rt" {
   }
 
   tags = {
-    Name = "example"
+    Name = "public-rt"
   }
+}
+
+
+# Creates association between public subnets and public route table
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public-subnet-2.id
+  route_table_id = aws_route_table.public-rt.id
 }
